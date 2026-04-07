e2f8a626362e5bca804b3be10ecc18238c6e653f
steakteppei <aki@teppei-usa.com>
2026-04-06 14:14:04 -1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,20 @@
-const CACHE = 'st-order-v1';
-const FILES = ['./', './index.html'];
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
-  );
+const CACHE='st-order-v2';
+const FILES=['./','./index.html','./send.html'];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-    })
-  );
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).then(function(res) {
-      var clone = res.clone();
-      caches.open(CACHE).then(function(c){c.put(e.request, clone);});
-      return res;
-    }).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+self.addEventListener('fetch',function(e){
+  e.respondWith(fetch(e.request).then(function(res){
+    var clone=res.clone();
+    caches.open(CACHE).then(function(c){c.put(e.request,clone);});
+    return res;
+  }).catch(function(){return caches.match(e.request);}));
 });
-
-self.addEventListener('message', function(e){ if(e.data&&e.data.type==='SKIP_WAITING') self.skipWaiting(); });
+self.addEventListener('message',function(e){if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
