ce35c8a6e6181fe6e946323740c7d3b7f13de5d2
steakteppei <aki@teppei-usa.com>
2026-04-10 04:49:40 -1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-// ★ アップデートのたびにこの番号を上げる（index.htmlのバージョンに合わせる）
-const CACHE='st-order-v3.0';
-
-const FILES=['./','./index.html','./send.html','./sales.html','./changelog.html'];
-
+const CACHE='st-order-v3.1';
+const FILES=['./', './index.html', './send.html', './sales.html', './changelog.html'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
   self.skipWaiting();
 });
-
 self.addEventListener('activate',function(e){
   e.waitUntil(caches.keys().then(function(keys){
     return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-  }).then(function(){
-    return self.clients.matchAll({type:'window'}).then(function(clients){
-      clients.forEach(function(client){client.postMessage({type:'SW_UPDATED'});});
-    });
   }));
   self.clients.claim();
 });
-
 self.addEventListener('fetch',function(e){
   e.respondWith(fetch(e.request).then(function(res){
     var clone=res.clone();
@@ -26,5 +17,4 @@ self.addEventListener('fetch',function(e){
     return res;
   }).catch(function(){return caches.match(e.request);}));
 });
-
 self.addEventListener('message',function(e){if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
