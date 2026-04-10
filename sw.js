ba741f999f2ed45f22cec74aca6d6dcc7cde5fa7
steakteppei <aki@teppei-usa.com>
2026-04-10 05:51:55 -1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='st-order-v3.1';
+const CACHE='st-order-v3.2';
 const FILES=['./', './index.html', './send.html', './sales.html', './changelog.html'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
